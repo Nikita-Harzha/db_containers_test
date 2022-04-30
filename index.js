aed2fd939892d7c98d23706e9288fa4f9699f4c4
Nikita Harzha <mharz@softserveinc.com>
2022-04-30 18:57:04 +0300

    Test commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('EASY PEASY LEMON SQUEAZY!!')
+console.log("EASY PEASY LEMON SQUEAZY!!");
+
+console.log("SECOND ATTEMPT");
+
+console.log("POSGRES_HOST", process.env.POSTGRES_HOST);
+console.log("POSTGRES PORT", process.env.POSTGRES_PORT);
